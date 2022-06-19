1d4d1f2e520bfd7e9a1ff4efa9a8b44c82b45a44
Luniam <mainul.i.mahi@gmail.com>
2022-06-19 17:05:43 +0600

    BloomFilter builder code changed

--- src/main/java/com/simplekv/db/IndexBloomFilter.java
+++ src/main/java/com/simplekv/db/IndexBloomFilter.java
@@ -13,41 +13,35 @@ import java.util.Map;
 
 public class IndexBloomFilter implements Serializable {
 
-
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    private BloomFilter<String> bloomFilter;
-    private String ssTableName;
-    private String indexFilename;
-
     static class IndexBloomFilterBuilder {
 
-        private IndexBloomFilter bloomFilter;
+        private BloomFilter<String> bloomFilter;
+        private String ssTableName;
+        private String indexFilename;
 
-        public IndexBloomFilterBuilder() {
-            this.bloomFilter = new IndexBloomFilter();
-        }
 
-        public IndexBloomFilter memTable(MemTableMBean memTable) {
+        public IndexBloomFilterBuilder() {}
+
+        public IndexBloomFilterBuilder memTable(MemTableMBean memTable) {
             int expectedInsertions = Math.toIntExact(memTable.getKeyCount()/2);
-            this.bloomFilter.setFilter(BloomFilter.create(
+            this.bloomFilter = BloomFilter.create(
                     Funnels.stringFunnel(StandardCharsets.UTF_8),
                     expectedInsertions,
-                    0.01));
+                    0.01);
             for(Map.Entry<KeyRecord, ValueRecord> entry : memTable.getMemData().entrySet()) {
-                this.bloomFilter.getFilter().put(entry.getKey().getKey());
+                this.bloomFilter.put(entry.getKey().getKey());
             }
-            return this.bloomFilter;
+            return this;
         }
 
-        public IndexBloomFilter sSTableName(String ssTableName) {
-            this.bloomFilter.ssTableName = ssTableName;
-            return this.bloomFilter;
+        public IndexBloomFilterBuilder sSTableName(String ssTableName) {
+            this.ssTableName = ssTableName;
+            return this;
         }
 
-        public IndexBloomFilter indexFileName(String indexFilename) {
-            this.bloomFilter.indexFilename = indexFilename;
-            return this.bloomFilter;
+        public IndexBloomFilterBuilder indexFileName(String indexFilename) {
+            this.indexFilename = indexFilename;
+            return this;
         }
 
         public IndexBloomFilter build() {
@@ -55,12 +49,19 @@ public class IndexBloomFilter implements Serializable {
         }
     }
 
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private BloomFilter<String> bloomFilter;
+    private String ssTableName;
+    private String indexFilename;
+
     public IndexBloomFilter() {}
 
     public IndexBloomFilter(IndexBloomFilterBuilder indexBloomFilterBuilder) {
-        this.bloomFilter = indexBloomFilterBuilder.bloomFilter.bloomFilter;
-        this.ssTableName = indexBloomFilterBuilder.bloomFilter.ssTableName;
-        this.indexFilename = indexBloomFilterBuilder.bloomFilter.indexFilename;
+        this.bloomFilter = indexBloomFilterBuilder.bloomFilter;
+        this.ssTableName = indexBloomFilterBuilder.ssTableName;
+        this.indexFilename = indexBloomFilterBuilder.indexFilename;
     }
 
     public void setFilter(BloomFilter<String> filter) {
